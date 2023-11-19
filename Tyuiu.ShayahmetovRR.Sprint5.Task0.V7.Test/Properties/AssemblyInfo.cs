30e66810829c6dea3781268d2bd2093d63929304
RaseWay <raseway@mail.ru>
2023-11-19 21:59:16 +0500

    Add project files.

---
+++ Tyuiu.ShayahmetovRR.Sprint5.Task0.V7.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShayahmetovRR.Sprint5.Task0.V7.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShayahmetovRR.Sprint5.Task0.V7.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4577ff9a-a46f-4ba3-8a52-26d20ef08202")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ShayahmetovRR.Sprint5.Task0.V7/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ShayahmetovRR.Sprint5.Task0.V7.Lib;
+
+namespace Tyuiu.ShayahmetovRR.Sprint5.Task0.V7
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			DataService ds = new DataService();
+			Console.Title = "Спринт #5 | Выполнил: Шаяхметов Р.Р | АСОиУб 23-1";
+			Console.WriteLine("******************************************************************************");
+			Console.WriteLine("* Спринт #5                                                                  *");
+			Console.WriteLine("* Тема: Обработка файлов                                                     *");
+			Console.WriteLine("* Задание #0                                                                 *");
+			Console.WriteLine("* Вариант #7                                                                 *");
+			Console.WriteLine("* Выполнил: Шаяхметов Р.Р     | АСОиУб 23-1                                  *");
+			Console.WriteLine("******************************************************************************");
+			Console.WriteLine("* УСЛОВИЕ:                                                                   *");
+			Console.WriteLine("* Вычислить выражение и сохранить результат в текстовый файл                 *");
+			Console.WriteLine("******************************************************************************");
+			Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                           *");
+			Console.WriteLine("******************************************************************************");
+
+			int x = 4;
+			Console.WriteLine("x = ", x);
+			
+			Console.WriteLine("******************************************************************************");
+			Console.WriteLine("* РЕЗУЛЬТАТ:                                                                 *");
+			Console.WriteLine("******************************************************************************");
+
+			string res = ds.SaveToFileTextData(x);
+
+			Console.WriteLine("Файл: " + res);
+			Console.WriteLine("Создан!");
+			Console.ReadKey();
+		}
+	}
+}
